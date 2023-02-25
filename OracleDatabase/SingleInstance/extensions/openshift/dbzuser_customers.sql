76a47db847197dbe53d2d7c0f226b3af51f777e4
Evan Zhang <evan.zhang@redhat.com>
2023-02-24 23:58:02 -0500

    Added data-sink and jdbc sink connector plugin

---
+++ OracleDatabase/SingleInstance/extensions/openshift/dbzuser_customers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customers (id number(9,0) primary key, name varchar2(50));
+INSERT INTO customers VALUES (1001, 'Salles Thomas');
+INSERT INTO customers VALUES (1002, 'George Bailey');
+INSERT INTO customers VALUES (1003, 'Edward Walker');
+INSERT INTO customers VALUES (1004, 'Anne Kretchmar');
+COMMIT;
