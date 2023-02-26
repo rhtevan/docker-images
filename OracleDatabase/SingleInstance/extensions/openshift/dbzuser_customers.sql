fab8b6f7fe19a779790681b9e01b7ddc8846f2ae
Evan Zhang <evan.zhang@redhat.com>
2023-02-25 23:06:22 -0500

    Completed e2e data pipepline from oracle11g to postgres

--- OracleDatabase/SingleInstance/extensions/openshift/dbzuser_customers.sql
+++ OracleDatabase/SingleInstance/extensions/openshift/dbzuser_customers.sql
@@ -3,4 +3,6 @@ INSERT INTO customers VALUES (1001, 'Salles Thomas');
 INSERT INTO customers VALUES (1002, 'George Bailey');
 INSERT INTO customers VALUES (1003, 'Edward Walker');
 INSERT INTO customers VALUES (1004, 'Anne Kretchmar');
+
+ALTER TABLE customers ADD SUPPLEMENTAL LOG DATA (ALL) COLUMNS;
 COMMIT;
